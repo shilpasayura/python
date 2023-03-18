6ebc212fa30cf68e36b98dc967fe79dd7bdeee79
Shilpa Sayura Foundation <shilpasayura@gmail.com>
2023-03-18 17:13:20 +0530

    Create cel2fa.py

---
+++ basic/cel2fa.py
@@ -0,0 +1,6 @@
+# Cel2Fahr Example
+# Convert Celsius temperature to Fahrenheit.
+
+cel = float(input("Enter a Celsius temperature: "))
+fahr = 9 * cel / 5 + 32
+print("Fahrenheit temperature:", fahr)
